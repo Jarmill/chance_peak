35340a85ca7c8a16308a11031cec16c5995e563a
Jared Miller <jared.fr.miller@gmail.com>
2022-12-31 17:07:27 -0500

    SDE experimentation on the Flow system

--- testing/sample/sde_motion_test_2d.m
+++ testing/sample/sde_motion_test_2d.m
@@ -4,7 +4,9 @@
 % b = -0.1;
 sigma = 0.1;
 F = @(t,X) [X(2); -(X(1) +X(2) + 0.5*X(1)^3)];
-G = @(t,X) sigma * [0; X(2)];
+% F = @(t,X) [X(2); -(X(1) +X(2) - (1/3)*X(1)^3)];
+% G = @(t,X) sigma * [0; X(2)];
+G = @(t,X) sigma * [0; 1];
 
 x0 = [1; 1];
 obj = sde(F, G, 'StartState', x0);    % dX = F(t,X)dt + G(t,X)dW
@@ -15,11 +17,14 @@ T =5;
 %Options
 Nperiod = ceil(T/dt);
 Antithetic = true;
-NTrials = 500;
+NTrials = 1000;
 
 [x_smp,t_smp] = simByEuler(obj, Nperiod, 'DeltaTime', dt, 'NTrials', NTrials,...
     'Antithetic', Antithetic);
 % x_smp = squeeze(x_smp);
+
+N_highlight = 4;
+c = linspecer(4);
 
 %% Plot
 figure(1)
@@ -27,14 +32,26 @@ clf
 hold on
 for i = 1:NTrials
 %     plot3(t_smp, x_smp(:, 1, i), x_smp(:, 2, i));
-    plot(x_smp(:, 1, i), x_smp(:, 2, i));
+%     plot(x_smp(:, 1, i), x_smp(:, 2, i));
+if i > (NTrials - N_highlight)
+    plot(x_smp(:, 1, i), x_smp(:, 2, i), 'color', c(NTrials-i+1, :), 'LineWidth', 2);
+else
+    plot(x_smp(:, 1, i), x_smp(:, 2, i), 'c');
+end
 end
 scatter(x0(1), x0(2), 200, 'ko')
 % titlestr = sprintf('dx = %0.2f x dt + %0.4f x dw', b, sigma);
-titlestr='2d dynamics';
-title(titlestr, 'fontsize', 16)
 
-xlabel('x_1(t)')
-ylabel('x_2(t)')
+xlabel('$x_1$', 'interpreter', 'latex')
+ylabel('$x_2$', 'interpreter', 'latex')
+title('Stochastic Flow System', 'FontSize', 14)
+
+plot(xlim, [1, 1]*[-0.874424669027551], 'r--', 'LineWidth', 3)
+plot(xlim, [1, 1]*-[1.16420596964602], 'r', 'LineWidth', 3)
+
+% [0.874424669027551;1.16420596964602]
+% 
+% plot(xlim, [1, 1]*[-0.509913216820256], 'r--', 'LineWidth', 3)
+% plot(xlim, [1, 1]*-[[0.950766054469061], 'r', 'LineWidth', 3)
 
 % xlim([0, T]);
