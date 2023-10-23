be711286a7ab8c8a66800a0b6e5f441449798117
Jared Miller <jared.fr.miller@gmail.com>
2023-10-23 13:08:54 -0400

    Update sde_geometric_growth_test.m

--- testing/sample/sde_geometric_growth_test.m
+++ testing/sample/sde_geometric_growth_test.m
@@ -19,9 +19,9 @@ T = 10;
 %Options
 Nperiod = ceil(T/dt);
 Antithetic = true;
-NTrials = 1000;
+% NTrials = 1000;
 % NTrials = 200;
-% NTrials = 500;
+NTrials = 500;
 
 [x_smp,t_smp] = simByEuler(obj, Nperiod, 'DeltaTime', dt, 'NTrials', NTrials,...
     'Antithetic', Antithetic);
@@ -33,34 +33,42 @@ clf
 hold on
 plot(t_smp, x_smp);
 
-titlestr = sprintf('dx = %0.2f x dt + %0.4f x dw', b, sigma);
-title(titlestr, 'fontsize', 16)
-xlabel('t')
-ylabel('x(t)')
+
+%% description
+titlestr = sprintf('$dx = %0.2f x(t) dt + %0.4f x(t) dw$', b, sigma);
+title(titlestr, 'fontsize', 16, 'interpreter', 'latex')
+xlabel('$t$', 'interpreter', 'latex', 'fontsize', 14)
+ylabel('$x(t)$', 'interpreter', 'latex', 'fontsize', 14)
+
+BOX = 1;
+
+if BOX
+    plot([4, 4, 8, 8, 4, 4], [3, 6, 6, 3, 3, 6], 'k', 'linewidth', 4)
+end
 
 xlim([0, T]);
 
-%% plot side-by-side
-
-figure(5)
-clf
-tiledlayout(1, 2)
-nexttile;
-hold on
-plot(t_smp, x_smp);
-
-titlestr = sprintf('dx = %0.2f x dt + %0.4f x dw', b, sigma);
-title(titlestr, 'fontsize', 16)
-xlabel('t')
-ylabel('x(t)')
-
-xlim([0, T]);
-
-nexttile;
-surf(tt, xx, pX)
-xlabel('t')
-ylabel('x')
-zlabel('p_t(x)')
-title(titlestr, 'fontsize', 16)
-shading interp
-title('Probability Density', 'fontsize', 16)
+% %% plot side-by-side
+% 
+% figure(5)
+% clf
+% tiledlayout(1, 2)
+% nexttile;
+% hold on
+% plot(t_smp, x_smp);
+% 
+% titlestr = sprintf('dx = %0.2f x dt + %0.4f x dw', b, sigma);
+% title(titlestr, 'fontsize', 16)
+% xlabel('t')
+% ylabel('x(t)')
+% 
+% xlim([0, T]);
+% 
+% nexttile;
+% surf(tt, xx, pX)
+% xlabel('t')
+% ylabel('x')
+% zlabel('p_t(x)')
+% title(titlestr, 'fontsize', 16)
+% shading interp
+% title('Probability Density', 'fontsize', 16)
